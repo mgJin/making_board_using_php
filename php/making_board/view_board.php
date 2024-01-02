2fbcacbc965beb196915a1097fd7bcb2ca89e344
mgJin <jmg940701@gmail.com>
2024-01-02 14:58:56 +0900

    여러개 뿌리기

--- php/making_board/view_board.php
+++ php/making_board/view_board.php
@@ -3,31 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>게시판보기</title>
+    <title>Document</title>
 </head>
 <body>
-    
-    <?php 
-        //처음 들어왔다면 페이지 0으로, 페이지 누를 때마다 session 에 묶던가 get에 묶어서 보낸다.
-        //dbuser 가 현재 사용중인 client의 id로 
-        //select @rown~~~query 로 보여주기 (done)
-        //해당 글을 누를때 board 의 pk 가 할당이 되어있어서 어떤 글인지 볼 수 있게
-        $servername = "localhost";
-        $dbuser = "root";//client user 로 바꿀 필요있음
-        $password = "9094";
-        $dbname = "phpboard";
-        try{
-            $connect = mysqli_connect($servername,$dbuser,$password,$dbname);
-            $sql = 
-                "SELECT id,title 
-                    FROM (SELECT @ROWN:=@ROWN+1 AS rown,id,title 
-                            FROM board,(SELECT @ROWN:=0) TEMP) ROWBOARD 
-                    WHERE rown>=2 AND rown<=5";
-            $result = $connect->query($sql);
-            var_dump($result->fetch_all(MYSQLI_ASSOC)); 
-        }catch(mysqli_sql_exception $ex){
-            echo "디비실패".$ex->getMessage();
-        }
-    ?>
+    <?php echo $_GET["id"];?>
 </body>
 </html>
