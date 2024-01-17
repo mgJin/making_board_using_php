077549e206ccf2814c26801edb94d2b84ea6415e
mgJin <jmg940701@gmail.com>
2024-01-17 16:10:26 +0900

    bar_making

--- php/making_board/index.php
+++ php/making_board/index.php
@@ -3,73 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>전체게시판보기</title>
+    <title>Document</title>
 </head>
 <body>
-    
-    <?php 
-        //처음 들어왔다면 페이지 0으로, 페이지 누를 때마다 session 에 묶던가 get에 묶어서 보낸다.
-        //페이지수를 n, min max 를 5n+1 과 5n+5(done)
-        //페이지가 2까지밖에 없는데 3으로 넘어오면(url에 적어서) 최대페이지로 넘어가게 하면 된다.   
-        //dbuser 가 현재 사용중인 client의 id로 
-        //select @rown~~~query 로 보여주기 (done)
-        //해당 글을 누를때 board 의 pk 가 할당이 되어있어서 어떤 글인지 볼 수 있게(done)
-        $currentpage = 0;//현재 페이지
-        
-        $DIVIDENUM = 3;//한 번에 몇 개의 게시판이 보여지는 가
-        if(isset($_GET["page"])){
-            $currentpage = $_GET["page"];
-        }
-        
-        $MIN = $DIVIDENUM * $currentpage + 1;
-        $MAX = $DIVIDENUM * $currentpage + $DIVIDENUM;
-        //db 연결
-        $servername = "localhost";
-        $dbuser = "root";//client user 로 바꿀 필요있음
-        $password = "9094";
-        $dbname = "phpboard";
-        try{
-            $connect = mysqli_connect($servername,$dbuser,$password,$dbname);
-            $viewsql = 
-                "SELECT id,title 
-                    FROM (SELECT @ROWN:=@ROWN+1 AS rown,id,title 
-                            FROM board,(SELECT @ROWN:=0) TEMP) ROWBOARD 
-                    WHERE rown>='$MIN' AND rown<='$MAX'";
-            //전체 개수 조회 (검색추가 하려면 여기서 IF 로 분기태우자)
-            $countsql = 
-                "SELECT COUNT(id) FROM BOARD";
-            $results = $connect->query($viewsql);
-            $count_results = $connect->query($countsql);
-            // var_dump($results->fetch_all(MYSQLI_ASSOC));
-        }catch(mysqli_sql_exception $ex){
-            echo "디비실패".$ex->getMessage();
-        }
-        
-    ?>
-    
-    <?php
-    //반복문으로 a 태그 여러개  띄우기(done)
-    //a태그 부분 좀 더 깔끔하게 안될까?
-    //세로로 배열필요
-    foreach($results as $result){
-        $baseurl = "http://localhost:3000/view_board.php";
-        $url = $baseurl ."?id=".$result["id"];
-    ?>
-    <a href=<?php echo $url ?>><?php echo $result["title"] ?></a>
-    <?php } ?>
-    <!-- 이제 페이지 넘기는 것을 만들 차례-->
-    <!-- get으로 page를 넘기면 된다-->
-    
-    <a href = <?php echo htmlspecialchars($_SERVER["PHP_SELF"])."?page=1";?>>2</a>
-    <?php 
-    //최대 5까지만 뜨게 하고 > 로 이동가능하게
-        $count = $count_results->fetch_row();
-        echo "현재 전체 개수는 : ".$count[0];
-        if($count>=5){
-
-        }
-    ?>
-    
-
+    <?php echo $_GET["id"];?>
 </body>
 </html>
--- php/making_board/view_all_board.php
+++ php/making_board/view_all_board.php
@@ -1,51 +1,114 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>전체게시판보기</title>
 </head>
+
 <body>
-    
-    <?php 
-        //처음 들어왔다면 페이지 0으로, 페이지 누를 때마다 session 에 묶던가 get에 묶어서 보낸다.
-        //페이지수를 n, min max 를 5n+1 과 5n+5
-        //페이지가 2까지밖에 없는데 3으로 넘어오면(url에 적어서) 최대페이지로 넘어가게 하면 된다.   
-        //dbuser 가 현재 사용중인 client의 id로 
-        //select @rown~~~query 로 보여주기 (done)
-        //해당 글을 누를때 board 의 pk 가 할당이 되어있어서 어떤 글인지 볼 수 있게(done)
-        $currentpage = 0;
-        if(isset($_GET["page"])){
-            $currentpage = $_GET["page"];
-        }
-        $min = 5 * $currentpage + 1;
-        $max = 5 * $currentpage + 5;
-        $servername = "localhost";
-        $dbuser = "root";//client user 로 바꿀 필요있음
-        $password = "9094";
-        $dbname = "phpboard";
-        try{
-            $connect = mysqli_connect($servername,$dbuser,$password,$dbname);
-            $sql = 
-                "SELECT id,title 
+    <!-- 게시글 목록 보여주기-->
+    <?php
+    //페이지가 2까지밖에 없는데 3으로 넘어오면(url에 적어서) 최대페이지로 넘어가게 하면 된다.   
+    //dbuser 가 현재 사용중인 client의 id로 
+
+    $currentpage = 1; //현재 페이지와 맨 처음 들어왔을 때 보여지는 페이지
+    $DIVIDENUM = 3; //한 번에 몇 개의 게시글이 보여지는 가
+    if (isset($_GET["page"])) {
+        $currentpage = $_GET["page"];
+    }
+   
+    //db 연결
+    $servername = "localhost";
+    $dbuser = "root"; //client user 로 바꿀 필요있음
+    $password = "9094";
+    $dbname = "phpboard";
+    //전체 게시판 개수 조회
+    try{
+        $connect= mysqli_connect($servername, $dbuser, $password, $dbname);
+        $countsql =
+        "SELECT COUNT(id) FROM BOARD";
+        $count_results = $connect->query($countsql);
+    } catch(mysqli_sql_exception $ex){
+        echo "디비실패 ".$ex->getMessage();
+    }
+    $count = $count_results->fetch_row();
+    //게시판 글 수를 한번에 보여줘야할 수로 나누어서 maxbar 계산
+    $MAXBOARD = (int)$count[0]; //전체 게시판 글 수
+    $MAXNUM = (int)($MAXBOARD / $DIVIDENUM); //표시되어야 할 barnum의 최대 개수
+    $CALNUM = $currentpage - 1;//계산에서는 현재 페이지에서 -1 을 뺀 숫자를 이용
+    if($MAXNUM < $currentpage){
+        $CALNUM = $MAXNUM-1;
+    }
+    if($CALNUM<0){
+        $CALNUM = 0;
+    }
+    $MINROW = $DIVIDENUM * $CALNUM + 1; //현재 페이지에서 보여줄 게시글의 최소 row수
+    $MAXROW = $DIVIDENUM * $CALNUM + $DIVIDENUM; //현재 페이지에서 보여줄 게시글의 최대 row수
+
+    //해당 페이지 게시글 조회
+    try {
+        $connect = mysqli_connect($servername, $dbuser, $password, $dbname);
+        $viewsql =
+            "SELECT id,title 
                     FROM (SELECT @ROWN:=@ROWN+1 AS rown,id,title 
                             FROM board,(SELECT @ROWN:=0) TEMP) ROWBOARD 
-                    WHERE rown>='$min' AND rown<='$max'";
-            $results = $connect->query($sql);
-            // var_dump($results->fetch_all(MYSQLI_ASSOC));
-        }catch(mysqli_sql_exception $ex){
-            echo "디비실패".$ex->getMessage();
-        }
+                    WHERE rown>='$MINROW' AND rown<='$MAXROW'";
+        //전체 개수 조회 (검색추가 하려면 여기서 IF 로 분기태우자)
+       
+        $results = $connect->query($viewsql);
+    } catch (mysqli_sql_exception $ex) {
+        echo "디비실패" . $ex->getMessage();
+    }
     ?>
-    
+
+    <!-- 게시글 목록만들기 -->
     <?php
-    //반복문으로 a 태그 여러개  띄우기(done)
+    //게시글 목록에서 클릭 시 게시글에 연결
     //a태그 부분 좀 더 깔끔하게 안될까?
-    foreach($results as $result){
+    //세로로 배열필요
+    foreach ($results as $result) {
         $baseurl = "http://localhost:3000/view_board.php";
-        $url = $baseurl ."?id=".$result["id"];
+        $url = $baseurl . "?id=" . $result["id"];
     ?>
-    <a href=<?php echo $url ?>><?php echo $result["title"] ?></a>
+        <a href=<?php echo $url ?>><?php echo $result["title"] ?></a>
     <?php } ?>
+
+    <!-- 이제 페이지 넘기기-->
+    <?php
+    
+    $BARRANGE = 5; //한 번에 보여줄 BARNUMBER의 개수
+    
+    if(!($MAXBOARD%$DIVIDENUM===0)){
+        $MAXNUM+=1;
+    }
+    $prevpage = $currentpage - 1;
+    $nextpage = $currentpage + 1;
+    if($nextpage>=$MAXNUM){
+        $nextpage = $MAXNUM;
+    }
+    //최대값 넘어가면 최대값으로 고정
+    $SHARE = (int)($CALNUM / $BARRANGE);
+    //barnum 보여주는 반복문
+    for ($i = 0; $i < $BARRANGE; $i++) {
+        $barnum = 5 * $SHARE + $i + 1;
+        if ($MAXNUM < $barnum) {
+            echo "최대값을 넘어가려함";
+            break;
+        }
+
+    ?>
+        <a href=<?php echo htmlspecialchars($_SERVER["PHP_SELF"]) . "?page=" . $barnum; ?>><?php echo $barnum; ?>&nbsp;</a>
+
+    <?php } ?>
+
+    <!-- 화살표로 숫자넘어가기 필요-->
+    <a href= <?php echo htmlspecialchars($_SERVER["PHP_SELF"])."?page=1";?>><<</a>
+    <a href= <?php echo htmlspecialchars($_SERVER["PHP_SELF"])."?page=".$prevpage;?>><</a>
+    <a href= <?php echo htmlspecialchars($_SERVER["PHP_SELF"])."?page=".$nextpage;?>>></a>
+    <a href= <?php echo htmlspecialchars($_SERVER["PHP_SELF"])."?page=".$MAXNUM?>>>></a>
+    <!-- 상수들 합치기 -->
 </body>
+
 </html>
