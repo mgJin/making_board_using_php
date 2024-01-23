19c556c96c59d46bb351b444e52981555e1361ba
mgJin <jmg940701@gmail.com>
2024-01-23 22:58:47 +0900

    모달창 만들기로 함

--- php/making_board/view_board.php
+++ php/making_board/view_board.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>게시글 보기</title>
+    <script src="https://code.jquery.com/jquery-3.7.0.js"></script>
 </head>
 <body>
     <!--권한 제한된 유저를 만들어서 그놈이 보는걸로 -->
@@ -16,37 +17,31 @@
         $stmt = $connect->query($sql);
         $result = $stmt->fetch(PDO::FETCH_ASSOC);    
     ?>
-    <?php 
-        if(array_key_exists("update",$_POST)){
-
-            $beh = "update";
-            echo chkbutton($beh,$_POST["$beh"]);
-        }else if(array_key_exists("delete",$_POST)){
-            $beh = "delete";
-            echo chkbutton($beh,$_POST["$beh"]);
-        }
-
-        
-        function chkbutton(string $beh,string $hangul){
-    
-            $ev = "var chk= confirm('$hangul 하시겠습니까?');
-                   if(chk){
-                    var php = '$beh'+'_board.php';
-                    location = php;}"
-                    ;
-            return "<script>$ev</script>";
-        }
-    ?>
+   
     <div>
         <h2>제목 : <?php echo $result["title"] ?></h2>
         <span>작성자 : <?php echo $result["writer"]?></span>
         <br>
         <span><?php echo $result["text"]?></span>
     </div>
-    <form method="post">
-        <input type="submit" name = "update" value = "수정">
-        <input type="submit" name = "delete" value = "삭제">
+    <form id="btn_form">
+        <button class="crdbtn" id="updbtn" value=<?php $board_id?>>수정</button>
+        <button class="crdbtn" id="delbtn" value=<?php $board_id?>>삭제</button>
     </form>
-    
+    <script>
+        $(".crdbtn").on("click",function(event){
+            const beh = event.target.innerText;
+            let chk = confirm(beh+"하시겠습니까?",function(result){
+                if(result){
+                    $("#btn_form").attr("action","delete_board.php");
+                    $("#btn_form").submit();
+                }else{
+                    window.location.replace("http://www.naver.com");
+                }
+            });
+           
+           
+        })
+    </script>
 </body>
 </html>
