a4bb3342ed8d2c1d02de9f45916acba34ebc0f90
mgJin <jmg940701@gmail.com>
2024-03-07 21:22:44 +0900

    adminpermission

--- php/making_board/Admin/RolePermission.php
+++ php/making_board/Admin/RolePermission.php
@@ -7,9 +7,12 @@
     <style>
         .container {
             width: 400px;
+            
+        }
+        form{
+            width:100%;
             display: flex;
         }
-
         .wrap-box {
             flex: 1;
             margin: 10px;
@@ -54,34 +57,67 @@
     //permissions 들을 여럿 선택할 수 있고 그 선택한 것들이 바로 permission_role table에 저장
     ?>
     <div class="container">
-        <div class="wrap-box">
-            <p>Roles</p>
-            <ul class="RoleBox">
-                <li>Admin</li>
-                <li>Author</li>
-                <li>Logger</li>
-                <li>Child</li>
-            </ul>
-        </div>
-        <div class="wrap-box">
-            <p>Permissions</p>
-            <ul>
-                <li>canCreateUser<input type="checkbox"></li>
-                <li>canUpdateUser<input type="checkbox"></li>
-                <li>canDeleteUser<input type="checkbox"></li>
-                <li>canCreateBoard<input type="checkbox"></li>
-                <li>canUpdateBoard<input type="checkbox"></li>
-                <li>canDeleteBoard<input type="checkbox"></li>
-                <li>canCreateRole<input type="checkbox"></li>
-                <li>canUpdateRole<input type="checkbox"></li>
-                <li>canDeleteRole<input type="checkbox"></li>
-            </ul>
-        </div>
+        <form id="rolepermission-form">
+            <div class="wrap-box">
+                <p>Roles</p>
+                <ul class="role-box">
+                    <li>Admin</li>
+                    <li>Author</li>
+                    <li>Logger</li>
+                    <li>Child</li>
+                </ul>
+            </div>
+            <div class="wrap-box">
+                <p>Permissions</p>
+                <ul class="permission-box">
+                    <li>canCreateUser<input type="checkbox" name="create-user"></li>
+                    <li>canUpdateUser<input type="checkbox" name="update-user"></li>
+                    <li>canDeleteUser<input type="checkbox"></li>
+                    <li>canCreateBoard<input type="checkbox"></li>
+                    <li>canUpdateBoard<input type="checkbox"></li>
+                    <li>canDeleteBoard<input type="checkbox"></li>
+                    <li>canCreateRole<input type="checkbox"></li>
+                    <li>canUpdateRole<input type="checkbox"></li>
+                    <li>canDeleteRole<input type="checkbox"></li>
+                </ul>
+            </div>
+        </form>
     </div>
     <div class="btn">
         <button >+</button>
+        <button id="upd-btn">update</button>
     </div>
     <script>
+        const pb = document.querySelector(".permission-box");
+        pb.addEventListener("click",function(event){
+            if(event.target.tagName=="LI"){
+                let check = event.target.querySelector('input[type="checkbox"]');
+                check.checked = !check.checked;
+            }
+        })
+        const updbtn = document.querySelector("#upd-btn");
+        updbtn.addEventListener("click",function(){
+
+            // formdata={
+            //     ccu:document.querySelector("input[name='create-user']").value,
+            //     cuu:document.querySelector("input[name='update-user']").value
+            // }
+            const fo = document.querySelector("#rolepermission-form");
+            let formdata = new FormData(fo);
+            postform(formdata);
+            
+        })
+
+        async function postform(data){
+            await fetch("http://localhost:3000/EXP.php",{
+                method:"POST",
+                body:data
+            })
+            .then(response=>response.json())
+            .then(data=>console.log("성공",data))
+            .catch(error=>console.log(error));
+           
+        }   
         
     </script>
 </body>
